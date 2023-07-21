c89ce3d6542f414499fa00aa42654b747c90c2f9
sanskaraggarwal2025 <aggarwalsanskar992@gmail.com>
2023-07-21 09:51:40 +0530

    modified start_container

--- start_container.sh
+++ start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-echo
+docker pull sanskaraggarwal2021/simple-python-flask-app 
 
 # Run the Docker image as a container
-echo
+docker run -d -p 5000:5000 sanskaraggarwal2021/simple-python-flask-app
